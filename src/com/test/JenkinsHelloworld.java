66be84a5c1d99e13f4698d8f3bc2f9f2225d51f0
sbviji <vijisb27@gmail.com>
2022-02-27 16:22:27 +0530

    Change main method

--- src/com/test/JenkinsHelloworld.java
+++ src/com/test/JenkinsHelloworld.java
@@ -3,7 +3,7 @@ package com.test;
 public class JenkinsHelloworld {
 	
 	
-	public static void main(String args[]) {
+	public static void main(String[] args) {
 		System.out.println("Jenkins Hello worldss");
 		
 	}
